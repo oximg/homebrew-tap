8b9e7a9a69457afb885c52bda110c7092592d53b
Lawrence Lin <deduce@gmail.com>
2026-07-04 09:57:34 -0500

    oximg 0.1.0 formula

---
+++ Formula/oximg.rb
@@ -0,0 +1,25 @@
+class Oximg < Formula
+  desc "Fast image compression and resizing: JPEG, PNG, WebP"
+  homepage "https://github.com/oximg/oximg"
+  url "https://github.com/oximg/oximg/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "92a8f48b6f7f9babcc2aefe0507a13f5de70720405655e50f10ebf5057ab7567"
+  license "Apache-2.0"
+  head "https://github.com/oximg/oximg.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "nasm" => :build
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    port = free_port
+    pid = spawn({ "PORT" => port.to_s, "IMAGES_DIR" => testpath.to_s }, bin/"oximg")
+    sleep 2
+    assert_match "ok", shell_output("curl -s http://127.0.0.1:#{port}/health")
+  ensure
+    Process.kill("TERM", pid)
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# oximg Homebrew Tap
+
+```sh
+brew install oximg/tap/oximg
+```
+
+Fast image compression and resizing in Rust. Source and documentation:
+https://github.com/oximg/oximg
